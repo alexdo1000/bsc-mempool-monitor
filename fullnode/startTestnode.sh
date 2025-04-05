08410e7506364da956cbf447d1db1adc92b6669b
Ubuntu <ubuntu@ip-172-31-11-167.ap-southeast-1.compute.internal>
2025-04-05 15:08:25 +0000

    Fixed testnet full node not starting websocket and added testnet start monitoring script

--- fullnode/startTestnode.sh
+++ fullnode/startTestnode.sh
@@ -1,8 +1,10 @@
-geth --config /home/ubuntu/bsc-mempool-monitor/fullnode/testnet/config.toml \
-  --datadir /home/ubuntu/snapshots/testnet/server/data-seed \
-  --cache 8000 \
-  --cache.preimages \
-  --snapshot=false \
-  --syncmode snap \
-  --state.scheme path \
-  --maxpeers 200
+# geth --config /home/ubuntu/bsc-mempool-monitor/fullnode/testnet/config.toml \
+#   --datadir /home/ubuntu/snapshots/testnet/server/data-seed \
+#   --cache 8000 \
+#   --cache.preimages \
+#   --snapshot=false \
+#   --syncmode snap \
+#   --state.scheme path \
+#   --maxpeers 200
+
+geth --config /home/ubuntu/bsc-mempool-monitor/fullnode/testnet/config.toml --datadir /home/ubuntu/snapshots/testnet/server/data-seed -cache 8000 --cache.preimages --snapshot=false --syncmode snap --state.scheme path --maxpeers 200 --ws --ws.addr 0.0.0.0 --ws.port 8576 --ws.api eth,net,web3,txpool
---
+++ testnetStart.sh
@@ -0,0 +1,1 @@
+./target/release/bsc-mempool-monitor --ws ws://localhost:8576
