426cc0725e447873c90fd2296a3b8448cf6f47e2
Ubuntu <ubuntu@ip-172-31-11-167.ap-southeast-1.compute.internal>
2025-04-05 18:13:43 +0000

    Optimized local test node configs for performance

--- fullnode/startTestnode.sh
+++ fullnode/startTestnode.sh
@@ -7,4 +7,17 @@
 #   --state.scheme path \
 #   --maxpeers 200
 
-geth --config /home/ubuntu/bsc-mempool-monitor/fullnode/testnet/config.toml --datadir /home/ubuntu/snapshots/testnet/server/data-seed -cache 8000 --cache.preimages --snapshot=false --syncmode snap --state.scheme path --maxpeers 200 --ws --ws.addr 0.0.0.0 --ws.port 8576 --ws.api eth,net,web3,txpool
+# geth --config /home/ubuntu/bsc-mempool-monitor/fullnode/testnet/config.toml --datadir /home/ubuntu/snapshots/testnet/server/data-seed -cache 8000 --cache.preimages --snapshot=false --syncmode snap --state.scheme path --maxpeers 200 --ws --ws.addr 0.0.0.0 --ws.port 8576 --ws.api eth,net,web3,txpool
+
+# Performance optimized geth startup script
+geth --config /home/ubuntu/bsc-mempool-monitor/fullnode/testnet/config.toml \
+--datadir /home/ubuntu/snapshots/testnet/server/data-seed \
+--cache 16000 \
+--cache.preimages \
+--snapshot=false \
+--syncmode snap \
+--state.scheme path \
+--ws \
+--ws.addr 0.0.0.0 \
+--ws.port 8576 \
+--ws.api eth,net,web3,txpool \
---
+++ fullnode/stopGeth.sh
@@ -0,0 +1,1 @@
+kill -SIGTERM 8068
